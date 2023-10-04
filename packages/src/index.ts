dfa5fbd5daeeb62db8f9c829bd5f9b91f36d3f70
Cat McGee <helloworld@mcgee.cat>
2023-10-04 21:41:06 +0100

    put it in index

--- packages/src/index.ts
+++ packages/src/index.ts
@@ -1,7 +1,8 @@
+
 import * as ethers from 'ethers';
-import { createPXEClient, getSandboxAccountsWallets, Contract } from '@aztec/aztec.js';
+import { createPXEClient, getSandboxAccountsWallets, Contract, Fr } from '@aztec/aztec.js';
+import {TokenBridgeContract} from './utils/TokenBridge.js';
 import { deployContract } from './utils/l1_contracts.js';
-import TokenBridgeContractAbi from '../aztec-contracts/token_bridge/target/TokenBridge.json' assert { type: "json" };
 
 const { SANDBOX_URL = 'http://localhost:8080' } = process.env;
 
@@ -10,13 +11,26 @@ async function main() {
     let signer = ethers.Wallet.fromMnemonic("test test test test test test test test test test test junk");
     signer = await signer.connect(provider);
     const tokenPortal = await deployContract(signer, "TokenPortal");
-    console.log(`deployed to ${tokenPortal.address}`);
-
+    console.log(`l1 contract deployed to ${tokenPortal.address}`);
 
     const client = createPXEClient(SANDBOX_URL);
     const [ownerWallet] = await getSandboxAccountsWallets(client);
     const ownerAddress = ownerWallet.getAddress();
-    console.log("owner address", ownerAddress.toString());
+    console.log("aztec owner address", ownerAddress.toString());
+
+    const accounts = await client.getRegisteredAccounts();
+    await console.log(accounts);
+
+    const deployerWallet = accounts[0];
+    const salt = Fr.random();
+
+    const tx = TokenBridgeContract.deploy(client).send({ contractAddressSalt: salt });
+    console.log(`Tx sent with hash ${await tx.getTxHash()}`);
+
+    await tx.isMined({ interval: 0.1 });
+    const receiptAfterMined = await tx.getReceipt();
+    console.log(`Status: ${receiptAfterMined.status}`);
+    console.log(`Contract address: ${receiptAfterMined.contractAddress}`);
 }
 
 main().catch(err => {
